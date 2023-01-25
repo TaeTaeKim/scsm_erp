0cdcc16ba2ec00e4ed1819b878814d66ffa418c4
TaeTaeKim <hc07car@gmail.com>
2023-01-25 13:30:45 +0900

    applying jinja template ;

--- app/main.py
+++ app/main.py
@@ -1,14 +1,29 @@
 import uvicorn
 
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request,Response
 from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+from jinja2 import Environment, FileSystemLoader
 
 app = FastAPI()
+app.mount('/static',StaticFiles(directory='static'),name='static')
+templates = Environment(
+    loader=FileSystemLoader("templates")
+)
+
+app.template_env = templates
 
 @app.get('/')
 def index():
-    return FileResponse('templates/index.html')
+    template = app.template_env.get_template('login.html')
+    return Response(content=template.render(), media_type="text/html")
+
+
+@app.get('/stock_list')
+def stock_list():
+    template = app.template_env.get_template('stockList.html')
+    return Response(content=template.render(user='admin'), media_type="text/html")
 
 
 if __name__ =="__main__":
-    uvicorn.run(app)
+    uvicorn.run(app,debug =True)
