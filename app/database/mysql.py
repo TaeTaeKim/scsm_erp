2f6d15f306df4acf6fc4f6fea8877ea386eba9c2
TaeTaeKim <hc07car@gmail.com>
2023-02-01 17:56:07 +0900

    add mysql connect file

---
+++ app/database/mysql.py
@@ -0,0 +1,24 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+import json
+
+with open("/Users/tykim/stock_management/app/database/credential.json") as file:
+    credential = json.load(file)
+
+SQLALCHEMY_DATABASE_URL_DATA = f"mysql+pymysql://{credential['mysql']['user']}:{credential['mysql']['password']}@{credential['mysql']['host']}:{credential['mysql']['port']}/{credential['mysql']['database']}"
+
+data_engine = create_engine(SQLALCHEMY_DATABASE_URL_DATA)
+
+data_session_local = sessionmaker(autocommit=False, autoflush=False, bind=data_engine)
+
+data_base = declarative_base()
+
+
+def get_db():
+    db = data_session_local()
+    try:
+        yield db
+    finally:
+        db.close()
