e81e1bca3bddb7f815d37828741fc3cbe5f2ac0b
TaeTaeKim <hc07car@gmail.com>
2023-02-03 16:20:47 +0900

    modify sql structure

--- app/StockMange.sql
+++ app/StockMange.sql
@@ -1,40 +1,46 @@
 CREATE TABLE items(
     item_code INT PRIMARY KEY,
     item_name VARCHAR(50) NOT NULL,
-    item_remain INT NOT NULL,
+    item_stock FLOAT(7,2) NOT NULL,
     item_unit VARCHAR(50) NOT NULL,
+    item_manufact VARCHAR(50),
     item_phone VARCHAR(50),
     item_email VARCHAR(50),
     item_img VARCHAR(255),
-    item_manufact VARCHAR(50),
     item_price INT,
     item_descript TEXT
 );
 
 CREATE TABLE orders(
-    order_id INT AUTO_INCREMENT PRIMARY KEY,
-    order_item INT,
-    order_num INT,
-    order_unit VARCHAR(50),
+    order_id VARCHAR(50) PRIMARY KEY,
+    order_item INT NOT NULL,
+    order_status INT NOT NULL DEFAULT 0,
+    order_num FLOAT(7,2) NOT NULL,
     order_request_date DATE,
-    order_placeorder BOOLEAN,
-    order_placeorder_date DATE,
+    order_purchase_date DATE,
     order_instock_date DATE,
-    order_instock BOOLEAN,
 
-    CONSTRAINT FOREIGN KEY(order_item) REFERENCES items(item_code)
-
-    ON DELETE CASCADE
+    CONSTRAINT FOREIGN KEY(order_item) REFERENCES items(item_code) ON DELETE CASCADE
     
+);
+CREATE TABLE usages(
+    usage_id INT AUTO_INCREMENT PRIMARY KEY,
+    usage_item INT NOT NULL,
+    usage_num FLOAT(7,2) NOT NULL,
+    usage_date DATE NOT NULL,
+
+    CONSTRAINT FOREIGN KEY(usage_item) REFERENCES items(item_code) ON DELETE CASCADE
+
 );
 
 CREATE TABLE accounts(
     account_id VARCHAR(10) PRIMARY KEY,
-    account_name VARCHAR(10) NOT NULL,
-    account_passwd VARCHAR(255),
-    account_order BOOLEAN ,
-    account_instock BOOLEAN,
-    account_item BOOLEAN,
-    account_management BOOLEAN
+    account_name VARCHAR(10) NOT NULL UNIQUE,
+    account_passwd VARCHAR(255) NOT NULL,
+    account_order BOOLEAN NOT NULL DEFAULT 0,
+    account_instock BOOLEAN NOT NULL DEFAULT 0,
+    account_item BOOLEAN NOT NULL DEFAULT 0,
+    account_management BOOLEAN NOT NULL DEFAULT 0,
+    account_session_id VARCHAR(255)
     
 );
